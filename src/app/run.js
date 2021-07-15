6110f984f2e41d9bac490320c8d01e0bfa4eadec
d4nielj <d4niel.djm@gmail.com>
2021-07-14 22:22:34 -0500

    Add extra methods to class todoList

--- src/app/run.js
+++ src/app/run.js
@@ -1,4 +1,4 @@
-import deployList from './components/reorderList.js';
+import deployList from './components/deployList.js';
 import todoList from './utils/todoList.js';
 
 export default function run() {
@@ -6,6 +6,8 @@ export default function run() {
   todoList.addTask('Create classes');
   todoList.addTask('Implement functionality');
   todoList.addTask('Check for errors with linters');
+  todoList.addTaskAt('Second place', 1);
+  console.log(todoList.arr);
   deployList(todoList);
   // grabberEvents();
 }
--- src/app/utils/todoList.js
+++ src/app/utils/todoList.js
@@ -12,10 +12,33 @@ class TodoList {
   };
 
   getTask = (index) => {
-    const [task] = this.arr.filter((task) => task.index === index);
+    // const [task] = this.arr.filter((task) => task.index === index);
+    const task = this.arr[index];
     return task;
+  }
+
+  removeTask = (index) => {
+    const [removedTask] = this.arr.filter((task) => task.index === index);
+    this.arr.filter((task) => task.index !== index);
+    return removedTask;
+  }
+
+  addTaskAt = (desc, index) => {
+    const newTask = new Task(this.index, desc);
+    this.arr.splice(index, 0, newTask);
   }
 }
 
 const todoList = new TodoList();
 export { todoList as default };
+
+ // if (index === 0) {
+    //   this.arr = [task].concat(this.arr);
+    // } else if (index === this.arr.length) {
+    //   this.arr = this.arr.concat([task]);
+    // } else {
+    //   let newArr = [];
+    //   for (let i = 0; i < index; i += 1) {
+    //     newArr = newArr.concat(this.arr[i])
+    //   }
+    // }
