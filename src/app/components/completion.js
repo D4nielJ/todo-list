7198e1e683b1fa39cf9477552f87906e8e839d3f
d4nielj <d4niel.djm@gmail.com>
2021-07-15 10:59:08 -0500

    Change listener from checkbox from click to change

--- src/app/components/completion.js
+++ src/app/components/completion.js
@@ -3,9 +3,8 @@ import todoList from "../utils/todoList";
 export default function completion() {
   const checkboxes = Array.from(document.querySelectorAll('.todo-list__completed'));
   checkboxes.forEach((box) => {
-    box.addEventListener('click', () => {
+    box.addEventListener('change', () => {
       todoList.completeTask(checkboxes.indexOf(box));
-      console.log(todoList.arr);
     })
   })
 }
