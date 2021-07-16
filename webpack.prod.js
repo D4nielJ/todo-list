c119aa46b6b3dc1f723671308d13cc7abd3671ce
d4nielj <d4niel.djm@gmail.com>
2021-07-16 13:16:25 -0500

    Add optimization for css

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -33,6 +35,12 @@ module.exports = merge(common, {
         test: /\.html$/i,
         loader: 'html-loader',
       },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
     ],
   },
 });
