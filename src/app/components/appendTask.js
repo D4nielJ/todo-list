27745f950cbeef1c89e7e62911a0a9c1282280a9
d4nielj <d4niel.djm@gmail.com>
2021-07-19 17:46:13 -0500

    Add destructuring to appendTask.js for cleaner code

--- src/app/components/appendTask.js
+++ src/app/components/appendTask.js
@@ -1,9 +1,10 @@
 export default function appendTask(index, todoList) {
   const li = document.createElement('li');
   const task = todoList.getTask(index);
+  const { desc, completed } = todoList.getTask(index);
   let checked = '';
   let classDesc = '';
-  if (task.completed === true) {
+  if (completed === true) {
     checked = 'checked';
     classDesc = 'todo-list__desc--done';
   }
@@ -18,18 +19,18 @@ export default function appendTask(index, todoList) {
       <p
         contenteditable="true"
         class="todo-list__desc ${classDesc}"
-      >${task.desc}</p>
+      >${desc}</p>
     </div>
     <div class="todo-list__grab grabber">
       <span class="grabber__circle"></span>
     </div>
     <button type="button" class="todo-list__delete fas fa-trash">
     </button>
-    <div data-index-target="${task.index + 1}" class="todo-list__target"></div>
+    <div data-index-target="${index + 1}" class="todo-list__target"></div>
     `;
   li.classList.add('todo-list__item');
-  li.setAttribute('data-index', `${task.index}`);
-  li.setAttribute('data-desc', `${task.desc}`);
+  li.setAttribute('data-index', `${index}`);
+  li.setAttribute('data-desc', `${desc}`);
   li.setAttribute('draggable', 'true');
   return li;
 }
