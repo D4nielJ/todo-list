f504fa82887f4b961c7985893e55bd58858a24d7
Rhyine <rhyinestewart@gmail.com>
2021-07-22 11:14:20 -0500

    Add test for the completion update

--- src/app/components/__tests__/editTask.test.js
+++ src/app/components/__tests__/editTask.test.js
@@ -4,7 +4,7 @@
 
  import todoList from '../../utils/todoList.js';
  import addTask from '../addTask.js';
- import deleteTask from '../deleteTask.js';
+import completion from '../completion.js';
  import editTask from '../editTask.js';
 
  
@@ -19,8 +19,7 @@
           addTask.addTask('This is a test.');
           
           const task = document.querySelector('p');
-          task.textContent = 'hello-world';
-          console.log(task.textContent);
+          task.textContent = 'This is an edited test.';
           const event = document.createEvent('HTMLEvents');
           event.initEvent('blur', true, true);
           event.eventName = 'blur';
@@ -30,6 +29,28 @@
         task.dispatchEvent(event);
 
            // Assert
-        expect(task.textContent).toBe('hello-world');
+        expect(task.textContent).toBe('This is an edited test.');
+     })
+
+     test('The description of the task should be correct', () => {
+        expect(todoList.arr[0].desc).toBe('This is an edited test.');
+      });
+ })
+
+ describe('Updating an item\'s status', () => {
+     test('Should update the completed status of an item.', () => {
+         //Arrange
+        const checkbox = document.querySelector('.todo-list__completed');
+        checkbox.setAttribute('checked', true); 
+        const event = document.createEvent('HTMLEvents');
+        event.initEvent('change', true, true);
+        event.eventName = 'change';
+
+         //Act
+        completion.addEvents();
+        checkbox.dispatchEvent(event);
+
+         //Assert
+         expect(todoList.arr[0].completed).toBe(true);
      })
  })
